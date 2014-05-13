ee7fe17c84079d0504eb53d0d3b6338fc3a3f63e
fedehacker <fede_rico_94@hotmail.com>
2014-05-14 00:09:34 +0200

    SQL updates properly located

---
+++ sql/updates/2014-05-14 Profesores.sql
@@ -0,0 +1,3 @@
+INSERT INTO `teachers` (`entry`, `dni`, `name`, `subname`, `email`, `pwd`) VALUES
+(1, '43654635g', 'Antonio', 'Garcia Gonzalez', 'gracioso@gmail.com', 'Antonio'),
+(2, '43543636g', 'Maria', 'Antonia Ramona', 'nosesae@gmail.com', 'Maria');
