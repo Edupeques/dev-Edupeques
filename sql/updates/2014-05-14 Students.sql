cd3abc8c93655eceed648910252e9281d652156e
desiraj <desiree.aban93@gmail.com>
2014-05-14 19:34:21 +0200

    [SQL] Update students - base for testing

---
+++ sql/updates/2014-05-14 Students.sql
@@ -0,0 +1,4 @@
+INSERT INTO `alumns` (`entry`, `matricula`, `name`, `subname`, `birthday`, `class`, `photo`, `pwd`) VALUES
+(1, 0, 'Luisa', 'Rubio', '2014-05-01', 31, 'buho.png', 123),
+(2, 0, 'Fernando', '', '2014-05-13', 31, 'burro.png', 234),
+(3, 0, 'Paula', 'Garcia', '2014-05-09', 31, 'caramelos.png', 345);
