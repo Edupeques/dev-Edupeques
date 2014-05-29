e4d9f75a590b561972b8e1043330f91d8fe71f04
Desiree <desiree.aban93@gmail.com>
2014-05-29 12:19:34 +0200

    Update game.php

--- proyecto/game.php
+++ proyecto/game.php
@@ -1,9 +1,71 @@
-<?php
-include("conexion.php");
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+    <HEAD>
+        <TITLE> Menu </TITLE>
+        <link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon" />
+        <link rel="stylesheet" href="../css/style.css">
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
+        <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+        <script>
+            $(function(){
+                $(".imageFace").click(function(){
+                    $("#entry").attr("value",$(this).attr("title"));
+                    $("#entrySubmit").submit();
+                });
+            });
+        </script>
+    </HEAD>
+    <BODY id="ninos">
+        <div id="cabecera">
+            <p>EDUPEQUES Educaci&oacute;n Infantil</p>
+        </div>	  
+        <div class="ninos">
+            <table style="padding:2%;">
+                <tr> 
+                    <?php
 session_start();
-if(!isset($_SESSION["entry"])) header("location:alumnos.php");
-// In construction
-die("SESION INICIADA");
+if(!isset($_GET["ref"])) header("location:index.html");
+if(isset($_GET["m"]) && $_GET["m"]==1) header("location:password.php?id=".$_GET["ref"]);
 
+$ref = $_GET["ref"];
+switch($ref)
+{
+    case 31: $ref = 1; break;
+    case 32: $ref = 2; break;
+    case 41: $ref = 3; break;
+    case 42: $ref = 4; break;
+    case 51: $ref = 5; break;
+    case 52: $ref = 6; break;
+    default: $ref = 1; break;
+}
+$i = 0;
+include("conexion.php");
 
-?>
+// obtenemos la edad recomendada del chaval a partir de su clase, no de su fecha de nacimiento
+$edad = $fsql->execute("SELECT edad FROM classes INNER JOIN alumns ON alumns.class = classes.entry WHERE alumns.entry = ?",array($_SESSION["entry"]))[0] 
+    or die("El alumno no existe o la clase no tiene una edad asignada");
+
+foreach($fsql->execute("SELECT * FROM categories") as $categoria)
+{
+    echo "<td colspan=5>".$categoria['name']."</td>";
+	if($row['entry']%5==0)
+        echo "</tr><tr>";
+    foreach($fsql->execute("SELECT * FROM games WHERE age = ?",array($edad)) as $juego)
+    {
+		 echo "<td><img src='img/".$row['image']."' width=100 onClick='parent.location=\"".$row['url']."\"' alt='".$row["game"]."' title='".$row["game"]."' class='imageFace, ninos'/></td>";
+		if($i==1)
+		{
+			$i = 0;
+			echo("<br />");
+		}
+    }
+
+	$i++;
+}
+                    ?>
+                </tr>
+            </table>
+        </div>
+    </BODY>
+</HTML>
