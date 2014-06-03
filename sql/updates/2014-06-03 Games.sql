39453e9176769f8fee9e50e1692f98f8ce1d3b15
desiraj <desiree.aban93@gmail.com>
2014-06-03 17:30:32 +0200

    Updated abecedario2 and insert Games

---
+++ sql/updates/2014-06-03 Games.sql
@@ -0,0 +1,63 @@
+﻿INSERT INTO  `edupeques`.`games` (
+`entry` ,
+`category`,
+`age`,
+`game`,
+`url`,
+`image`
+)
+VALUES (
+NULL ,  '6', '3', 'Letra A', '../minigames/vocales/vocalA.html', ''
+), (
+NULL ,  '6', '3', 'Letra E', '../minigames/vocales/vocalE.html', ''
+), (
+NULL ,  '6', '3', 'Letra I', '../minigames/vocales/vocali.html', ''
+), (
+NULL ,  '6', '3', 'Letra O', '../minigames/vocales/vocalO.html', ''
+), (
+NULL ,  '6', '3', 'Letra U', '../minigames/vocales/vocalU.html', ''
+), (
+NULL ,  '6', '4', 'Letra B', '../minigames/abecedario/Bmayus.html', ''
+), (
+NULL ,  '6', '4', 'Letra C', '../minigames/abecedario/Cmayus.html', ''
+), (
+NULL ,  '6', '4', 'Letra D', '../minigames/abecedario/Dmayus.html', ''
+), (
+NULL ,  '6', '4', 'Letra F', '../minigames/abecedario/Fmayus.html', ''
+), (
+NULL ,  '6', '4', 'Letra G', '../minigames/abecedario/Gmayus.html', ''
+), (
+NULL ,  '6', '4', 'Letra H', '../minigames/abecedario/Hmayus.html', ''
+), (
+NULL ,  '6', '4', 'Letra J', '../minigames/abecedario/Jmayus.html', ''
+), (
+NULL ,  '6', '4', 'Letra K', '../minigames/abecedario/Kmayus.html', ''
+), (
+NULL ,  '6', '4', 'Letra L', '../minigames/abecedario/Lmayus.html', ''
+), (
+NULL ,  '6', '4', 'Letra M', '../minigames/abecedario/Mmayus.html', ''
+), (
+NULL ,  '6', '5', Tabla del Abacedario', '../minigames/abecedario/abecedarioTabla.html', ''
+), (
+NULL ,  '6', '5', 'Letra N', '../minigames/abecedario2/Nmayus.html', ''
+), (
+NULL ,  '6', '5', 'Letra P', '../minigames/abecedario2/Pmayus.html', ''
+), (
+NULL ,  '6', '5', 'Letra Q', '../minigames/abecedario2/Qmayus.html', ''
+), (
+NULL ,  '6', '5', 'Letra R', '../minigames/abecedario2/Rmayus.html', ''
+), (
+NULL ,  '6', '5', 'Letra S', '../minigames/abecedario2/Smayus.html', ''
+), (
+NULL ,  '6', '5', 'Letra T', '../minigames/abecedario2/Tmayus.html', ''
+), (
+NULL ,  '6', '5', 'Letra V', '../minigames/abecedario2/Vmayus.html', ''
+), (
+NULL ,  '6', '5', 'Letra W', '../minigames/abecedario2/Wmayus.html', ''
+), (
+NULL ,  '6', '5', 'Letra X', '../minigames/abecedario2/Xmayus.html', ''
+), (
+NULL ,  '6', '5', 'Letra Y', '../minigames/abecedario2/Ymayus.html', ''
+), (
+NULL ,  '6', '5', 'Letra Z', '../minigames/abecedario2/Zmayus.html', ''
+);
